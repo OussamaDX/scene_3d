c32a0cf2611dcc8bb22bab0095bd433652fb9510
Oussama Dx <oussama.python.2022@gmail.com>
2024-06-22 03:41:35 +0100

    add bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ThreeScene from './ThreeScene';
+import './ThreeScene.css';
 
 const App: React.FC = () => {
   return (
--- src/ThreeScene.tsx
+++ src/ThreeScene.tsx
@@ -6,12 +6,18 @@ const ThreeScene: React.FC = () => {
 
   useEffect(() => {
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x87CEEB);
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     if (mountRef.current) {
       mountRef.current.appendChild(renderer.domElement); // apend child ex
     }
+    // const textureLoader = new THREE.TextureLoader();
+    // const bgTexture = textureLoader.load('/Users/OUShassan/Downloads/simon.jpg',(texture) => {
+    //   scene.background = texture;
+    // });
+    
     const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
